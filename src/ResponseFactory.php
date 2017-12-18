3809625458034ba5e831ba754e93780b3ade0cf2
Rik Meijer <rik@pulledbits.org>
2017-12-18 10:46:08 +0100

    extend responsefactory with makeWithTemplate

--- src/ResponseFactory.php
+++ src/ResponseFactory.php
@@ -2,10 +2,13 @@
 namespace pulledbits\Router;
 
 use Psr\Http\Message\ResponseInterface;
+use pulledbits\View\TemplateInstance;
 
 interface ResponseFactory
 {
     public function make(string $statusCode, string $body) : ResponseInterface;
 
     public function makeWithHeaders(string $statusCode, array $headers, string $body) : ResponseInterface;
+
+    public function makeWithTemplate(string $statusCode, TemplateInstance $templateInstance) : ResponseInterface;
 }
