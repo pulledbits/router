55a3f80557e9c230dde35e3f1c1b9c295e77a0bd
Rik Meijer <rik@pulledbits.org>
2019-03-19 10:15:32 +0100

    rename variables to reflect contents

--- src/Router.php
+++ src/Router.php
@@ -9,36 +9,36 @@ class Router
      */
     private $routes = [];
 
-    public function __construct(array $routes)
+    public function __construct(array $routeFactories)
     {
-        $this->routes = $routes;
+        $this->routes = $routeFactories;
     }
 
-    public function addPath(string $regexp, string $responseFactory) {
-        $this->addRoute($regexp, require $responseFactory);
+    public function addPath(string $regexp, string $routeFactoryPath) {
+        $this->addRoute($regexp, require $routeFactoryPath);
     }
 
-    public function addRoute(string $regexp, callable $route) : void
+    public function addRoute(string $regexp, callable $routeFactory) : void
     {
-        $reflection = new \ReflectionFunction($route);
+        $reflection = new \ReflectionFunction($routeFactory);
         if ($reflection->hasReturnType() === false) {
             return;
         } elseif ($reflection->getReturnType()->getName() !== Route::class) {
             return;
         }
-        $this->routes[$regexp] = $route;
+        $this->routes[$regexp] = $routeFactory;
     }
 
     public function route(\Psr\Http\Message\ServerRequestInterface $request): RouteEndPoint
     {
         $uri = $request->getUri();
-        foreach ($this->routes as $regexp => $responseFactory) {
+        foreach ($this->routes as $regexp => $routeFactory) {
             $matches = [];
             if (preg_match("#" . $regexp . "#", $uri->getPath(), $matches) === 1) {
                 foreach ($matches as $matchIdentifier => $match) {
                     $request = $request->withAttribute($matchIdentifier, $match);
                 }
-                return $responseFactory()->handleRequest($request);
+                return $routeFactory()->handleRequest($request);
             }
         }
         return ErrorFactory::makeInstance(404);
